c9424d59bd3d960a8ad44a642bf8742a3416859f
KrizTech <christiandonatus01@gmail.com>
2022-11-15 20:31:41 -0800

    header file

--- shell.h
+++ shell.h
@@ -1,39 +1,36 @@
-#ifndef SHELL_H
-#define SHELL_H
+#ifndef MAIN_H
+#define MAIN_H
 
+#include <sys/wait.h>
+#include <sys/types.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 #include <string.h>
-#include <signal.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <limits.h>
+#include <string.h>
 
-/* shell none interact mode functions */
-void non_interact_mode(char **lineptr,
-			size_t *no, char *sh_name, char **av, char **env);
-
-/* shell interact mode function */
-void interact_mode(char **lineptr, size_t *no,
-		char *sh_name, char **av, char **env);
-
-/* shell_util functionc */
-void handle_args(char *str, char **av);
-char *_testpath(const char *path, const char *command);
-char *_which(const char *command, char **env);
-void sig_handler(int signum);
-
-/* std_util functions */
-int _strtok_validate(char *str, char *backup, char *delm);
-char *_strtok(char *str, char *delm);
-int _strlen(const char *str);
-void exit_shell(int status, char *ptr);
-
-/* _env functions */
-int check_var(char *env, const char *name);
-char *getvalue(char *line);
-char *_getenv(const char *name, char **env);
-int _unsetenv(const char *name, char **env);
 extern char **environ;
 
-#endif /* SHELL_H */
+char *show_input(void);
+void prompt(void);
+char *_strcat(char *src);
+int _strlen(char *str);
+void place(char *str);
+char *findfile(char *command);
+char *find_command(char *command);
+int compare(char *s1, char *s2);
+int _strcmpdir(char *s1, char *s2);
+int charput(char c);
+void place(char *str);
+char *str_concat(char *s1, char *s2);
+int lookforslash(char *cmd);
+int compareExit(char *s1, char *s2);
+int compareEnv(char *s1, char *s2);
+void execute_proc(char **cmd);
+char **identify_string(char *parameter);
+void controlC(int sig);
+
+#endif
